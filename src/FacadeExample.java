d2272adc4bea107619962f831c71042d9bad5677
Prime <wolabash@gmail.com>
2025-03-21 14:06:34 +0000

    Feat: Structural Patterns (Facade Patterns)

---
+++ src/FacadeExample.java
@@ -0,0 +1,20 @@
+class AudioPlayer{
+    void playAudio(){
+        System.out.println("Playing Audio");
+    }
+}
+
+class VideoPlayer{
+    void playVideo(){
+        System.out.println("Playing Video");
+    }
+}
+
+class StreamingService{
+    void startStreaming(){
+        System.out.println("Streaming Online");
+    }
+}
+
+public class FacadeExample {
+}
